8d38eadadde2a3c7b88a109448e65858e47040b0
Baptistemontan <baptiste.demontangon@gmail.com>
2024-11-11 17:14:31 +0100

    removed "lang" attribute from the shell, already set by leptos_i18n

--- app/src/lib.rs
+++ app/src/lib.rs
@@ -17,7 +17,7 @@ leptos_i18n::load_locales!();
 pub fn shell(options: LeptosOptions) -> impl IntoView {
     view! {
         <!DOCTYPE html>
-        <html lang="en">
+        <html>
             <head>
                 <meta charset="utf-8"/>
                 <meta name="viewport" content="width=device-width, initial-scale=1"/>
